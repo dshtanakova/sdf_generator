7dbead81bd248c9edfab02c866d46d1cedb811a9
daria_3dlook <daria@3dlook.me>
2020-02-29 18:02:39 +0000

    test transfering mesh to sdf

--- tests/test_kaolin.py
+++ tests/test_kaolin.py
@@ -1,1 +1,7 @@
 import kaolin as kal
+
+
+if __name__ == "__main__":
+    mesh = kal.rep.TriangleMesh.from_obj('../data/0_simplified.obj')
+    sdf = kal.conversions.trianglemesh_to_sdf(mesh)
+    print(sdf)
